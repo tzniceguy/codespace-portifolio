2e3640876d7e7c602341c30684cbb045be2aaa88
Joe <52755372+tzniceguy@users.noreply.github.com>
2024-09-16 07:13:02 +0000

    changed the photo to mr bean from oixabay

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Joe",
+  title: "Software Developer/ Engineer",
+  email: "tzniceguy@gmail.com",
+  gitHub: "tzniceguy",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "tzniceguy",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
